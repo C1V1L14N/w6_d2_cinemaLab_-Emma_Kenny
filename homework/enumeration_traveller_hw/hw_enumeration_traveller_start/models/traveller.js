12e02e38977164aa6e387dc439458b4e82c1ebf5
Kenny <kenny.mckeown@gmail.com>
2020-11-18 20:49:05 +0000

    wk6 d3 HW complete

---
+++ homework/enumeration_traveller_hw/hw_enumeration_traveller_start/models/traveller.js
@@ -0,0 +1,44 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport)
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance > minDistance)
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   return this.journeys.map((journey) => {
+//     return new Set(journey.transport)
+//   })
+// };
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const unique = [...new Set(this.journeys.map(journey => journey.transport))];
+  return unique;
+  };
+
+
+
+module.exports = Traveller;
